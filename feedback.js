8dc5116bccce3b5f4adacf6056e9e589a8856fdc
ShatakshiDudala <shatakshidudala@gmail.com>
2025-07-09 09:58:57 +0530

    Delete feedback.js

--- feedback.js
+++
@@ -1,9 +0,0 @@
-function submitFeedback() {
-  const comment = document.getElementById('commentBox').value;
-  if (comment.trim().length > 0) {
-    document.getElementById('statusMsg').textContent = "✅ Thank you for your feedback!";
-    document.getElementById('commentBox').value = "";
-  } else {
-    document.getElementById('statusMsg').textContent = "⚠️ Please enter a comment before submitting.";
-  }
-}
